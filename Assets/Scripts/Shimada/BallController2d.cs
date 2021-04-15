74461b96a14b1efa74d926631300bea902c8b90b
RyutoShimada <business.sub10110@gmail.com>
2021-04-15 15:48:41 +0900

    最初のコミット

---
+++ Assets/Scripts/Shimada/BallController2d.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController2d : MonoBehaviour
+{
+    [SerializeField] float m_pushPower = 1f;
+    [SerializeField] GameObject m_selectBall = null;
+
+    Rigidbody2D m_rb;
+    public bool m_isSelect = false;
+
+    bool m_isSelectedBall = false;//縁取り用のボールが子オブジェクトとしてあるかどうか
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float random = Random.Range(-1f, 1f);
+        m_rb = GetComponent<Rigidbody2D>();
+        m_rb.AddForce(new Vector2(random, 0) * m_pushPower, ForceMode2D.Impulse);
+    }
+
+    void Update()
+    {
+        if (m_isSelect)
+        {
+            m_isSelectedBall = true;
+            Vector2 pos = transform.position;
+            Quaternion rotat = transform.rotation;
+            Instantiate(m_selectBall, pos, rotat, transform);
+        }
+        else
+        {
+            if (m_isSelectedBall)
+            {
+                Destroy(m_selectBall);
+                m_isSelectedBall = false;
+            }
+        }
+    }
+
+    void OnLuminous()
+    {
+
+    }
+
+    void UnKuminous()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Shimada/Chaincontroller.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chaincontroller : MonoBehaviour
+{
+    //[SerializeField] GameObject m_selectBall = null;
+
+    Vector3 m_ray;
+    RaycastHit2D m_hit;
+    List<GameObject> m_ballList;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_ballList = new List<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_ray = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (m_hit = Physics2D.Raycast(m_ray, Vector3.forward, 100f))
+            {
+                if (m_hit.collider.tag == "Ball")
+                {
+                    m_hit.collider.GetComponent<BallController2d>().m_isSelect = true;
+                    m_ballList.Add(m_hit.collider.gameObject);
+                    Debug.Log(m_ballList.Count);
+                    //Vector2 pos = m_hit.collider.transform.position;
+                    //Quaternion rotat = m_hit.collider.gameObject.transform.rotation;
+                    //Transform parent = m_hit.collider.gameObject.transform;
+                    //Instantiate(m_selectBall, pos, rotat, parent);
+                }
+            }
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            if (m_hit = Physics2D.Raycast(m_ray, Vector3.forward, 100f))
+            {
+                if (m_hit.collider.tag == "Ball")
+                {
+                    m_hit.collider.GetComponent<BallController2d>().m_isSelect = true;
+                    m_ballList.Add(m_hit.collider.gameObject);
+                    Debug.Log(m_ballList.Count);
+                    //Vector2 pos = m_hit.collider.transform.position;
+                    //Quaternion rotat = m_hit.collider.gameObject.transform.rotation;
+                    //Transform parent = m_hit.collider.gameObject.transform;
+                    //Instantiate(m_selectBall, pos, rotat, parent);
+                }
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            m_hit.collider.GetComponent<BallController2d>().m_isSelect = false;
+            if (m_ballList.Count >= 3)
+            {
+                foreach (var item in m_ballList)
+                {
+                    Destroy(item);
+                }
+            }
+            
+            m_ballList.Clear();
+        }
+    }
+}
