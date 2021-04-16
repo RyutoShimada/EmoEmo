e0bd7b37c2fa83fc95ba7ce3bc3a69172e28bcc9
ShinNakajima128 <ukauka224@i.softbank.jp>
2021-04-16 17:24:09 +0900

    Sceneを遷移する時に画面をフェードさせながら遷移するシステムを作成した。

---
+++ Assets/Scripts/Nakajima/SceneController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    [SerializeField] FadeController m_fc;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void LoadScene()
+    {
+        m_fc.isFadeOut = true;
+        StartCoroutine(LoadTimer());
+    }
+
+    IEnumerator LoadTimer()
+    {
+        yield return new WaitForSeconds(2.0f);
+
+        SceneManager.LoadScene("NakajimaScene");
+    }
+}
