92498d403ea36901699677b5fd74e1899df86c56
YoshiTa0527 <dfingao0527@gmail.com>
2021-04-17 02:36:58 +0900

    ボールの生成の挙動を修正した

--- Assets/Scripts/Tanaka/BallCounter.cs
+++ Assets/Scripts/Tanaka/BallCounter.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using System.Linq;
+using UnityEngine;
 /// <summary>
 /// ランダムなボールをこのスクリプトがアタッチされたオブジェクトの場所から生成する
 /// </summary>
@@ -17,33 +17,42 @@ public class BallCounter : MonoBehaviour
     float m_timer;
     /// <summary>ボールを生成する間隔</summary>
     [SerializeField] float m_interval = 1f;
+    /// <summary>最初のボールを生成する上限</summary>
+    [SerializeField] int m_firstMaxBallCount = 10;
     /// <summary>ボールを生成する上限</summary>
     [SerializeField] int m_maxBallCount = 3;
     /// <summary>最初に出すボールの数</summary>
-    [SerializeField] int m_firstBallCount = 10;
+    [SerializeField] int m_firstBallCountAtPool = 10;
     /// <summary>ボールのリスト</summary>
     List<BallController2d> m_childBalls = new List<BallController2d>();
+    /// <summary>一回目か否か</summary>
+    public bool m_IsFirstTime { get; set; }
 
     private void Start()
     {
-        for (int i = 0; i <= m_firstBallCount; i++)
+        m_IsFirstTime = true;
+        for (int i = 0; i <= m_firstBallCountAtPool; i++)
         {
             InstantiateBall();
         }
         m_childBalls = GetComponentsInChildren<BallController2d>().ToList();
         if (m_childBalls != null) { Debug.Log("get childBalls"); }
-        m_childBalls.ForEach(ball => ball.gameObject.SetActive(false));
+        //m_childBalls.ForEach(ball => ball.gameObject.SetActive(false));
+        ActivateBallAtFirst();
     }
 
     private void Update()
     {
-        if (m_childBalls.Where(ball => ball.gameObject.activeSelf == true).Count() < m_maxBallCount)
+        if (!m_IsFirstTime)
         {
-            m_timer += Time.deltaTime;
-            if (m_timer > m_interval)
+            if (m_childBalls.Where(ball => ball.gameObject.activeSelf == true).Count() < m_maxBallCount)
             {
-                m_timer = 0;
-                ActivateBall();
+                m_timer += Time.deltaTime;
+                if (m_timer > m_interval)
+                {
+                    m_timer = 0;
+                    ActivateBall();
+                }
             }
         }
     }
@@ -59,7 +68,6 @@ public class BallCounter : MonoBehaviour
         m_childBalls.Where(ball => ball.gameObject.activeSelf == true).ToList().ForEach(ball => ball.gameObject.SetActive(false));
     }
 #endif
-
     /// <summary>
     /// ボールをクローンする
     /// </summary>
@@ -67,7 +75,9 @@ public class BallCounter : MonoBehaviour
     {
         if (m_ballPrefabs == null) return;
         m_index = Random.Range(0, m_ballPrefabs.Length);
-        Instantiate(m_ballPrefabs[m_index], this.transform.position, Quaternion.identity, this.transform);
+        GameObject go = Instantiate(m_ballPrefabs[m_index], this.transform.position, Quaternion.identity, this.transform);
+        float randomPower = Random.Range(0, 3);
+        go.gameObject.GetComponent<Rigidbody2D>()?.AddForce(Vector2.up * randomPower, ForceMode2D.Impulse);
     }
 
     /// <summary>
@@ -80,10 +90,20 @@ public class BallCounter : MonoBehaviour
         if (m_clonePosA && m_clonePosB)
         {
             float clonePosX = Random.Range(m_clonePosA.transform.position.x, m_clonePosB.transform.position.x);
-            Debug.Log(clonePosX);
-            activeBall.gameObject.transform.position = new Vector2(clonePosX, this.transform.position.y);
+            float clonePosY = Random.Range(this.transform.position.y + 1f, this.transform.position.y - 1f);
+            activeBall.gameObject.transform.position = new Vector2(clonePosX, clonePosY);
         }
         else activeBall.gameObject.transform.position = this.transform.position;
         activeBall.gameObject.SetActive(true);
+    }
+
+    void ActivateBallAtFirst()
+    {
+        while (m_childBalls.Where(ball => ball.gameObject.activeSelf == true).Count() > m_firstMaxBallCount)
+        {
+            NonActiveBall();
+        }
+        m_IsFirstTime = false;
+        Debug.Log("ActivateBallAtFirst()");
     }
 }
