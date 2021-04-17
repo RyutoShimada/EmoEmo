cccaf3c24dc17b498dd2f5d21883f1b619ad71c7
yosho_cho <choyosho165152987@gmail.com>
2021-04-17 14:40:50 +0900

    メッセージのアニメーションを追加

--- Assets/Scripts/Yosho/messagecontroller.cs
+++ Assets/Scripts/Yosho/messagecontroller.cs
@@ -8,14 +8,14 @@ public class messagecontroller : MonoBehaviour
     Vector3 m_initialPosition;
     [SerializeField] Transform m_target = null;
 
+    [SerializeField] Transform m_EndPosition = null;
+
     [SerializeField] float m_speed = 0.5f;
 
-    Rigidbody2D m_rb;
-    // Start is called before the first frame update
     void Start()
     {
-        m_rb = GetComponent<Rigidbody2D>();
-        m_initialPosition = this.transform.position;
+       
+        //m_initialPosition = this.transform.position;
         Scrolling();
     }
 
@@ -30,7 +30,7 @@ public class messagecontroller : MonoBehaviour
         Sequence seq = DOTween.Sequence();
         seq.Append(this.transform.DOMove(m_target.position, 1f))
             .AppendInterval(2)
-            .Append(this.transform.DOMove(m_initialPosition, 3f))
+            .Append(this.transform.DOMove(m_EndPosition.position, 3f))
             .AppendInterval(3);
         //.OnComplete(() => { };
         seq.Play();
