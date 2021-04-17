4064d91f6383f811cabb71819052091320d130f8
RyutoShimada <business.sub10110@gmail.com>
2021-04-17 19:40:15 +0900

    チェインした時に線が出るようにした

--- Assets/Scripts/Shimada/Chaincontroller.cs
+++ Assets/Scripts/Shimada/Chaincontroller.cs
@@ -15,10 +15,18 @@ public class Chaincontroller : MonoBehaviour
     /// <summary>チェーン状態のボールが入る</summary>
     List<GameObject> m_ballList;
 
+    LineRenderer m_lineRenderer;
+    List<Vector3> m_linePosList;
+
     // Start is called before the first frame update
     void Start()
     {
         m_ballList = new List<GameObject>();
+        m_linePosList = new List<Vector3>();
+        m_lineRenderer = GetComponent<LineRenderer>();
+        m_lineRenderer.startWidth = 0.2f;
+        m_lineRenderer.endWidth = 0.2f;
+        m_lineRenderer.positionCount = 0;
     }
 
     // Update is called once per frame
@@ -27,11 +35,12 @@ public class Chaincontroller : MonoBehaviour
         m_rayOrigin = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         m_hit = Physics2D.Raycast(m_rayOrigin, Vector3.forward, 100f);
         GetMouseButton();
-        m_chainText.text = "CHAIN : " + m_ballList.Count.ToString();
     }
 
     void GetMouseButton()
     {
+        m_chainText.text = "CHAIN : " + m_ballList.Count.ToString();
+
         if (Input.GetMouseButton(0))
         {
             if (m_hit && m_hit.collider.tag == "Ball")
@@ -39,14 +48,15 @@ public class Chaincontroller : MonoBehaviour
                 if (m_ballList.Count == 0)//1個目のボール
                 {
                     m_ballList.Add(m_hit.collider.gameObject);
+                    m_linePosList.Add(m_hit.collider.gameObject.transform.position);
+                    m_lineRenderer.positionCount++;
                 }
                 else//2個目以上のボール
                 {
                     GameObject hitObj = m_hit.collider.gameObject;
-                    GameObject listFirstObj = m_ballList.First().gameObject;
                     GameObject listLastObj = m_ballList.Last().gameObject;
                     float distance = Vector2.Distance(hitObj.transform.position, listLastObj.transform.position);
-                    float diameter = hitObj.GetComponent<Renderer>().bounds.size.x + m_margin;
+                    float diameter = hitObj.GetComponent<Renderer>().bounds.size.x + (hitObj.GetComponent<Renderer>().bounds.size.x * m_margin);
 
                     //①既に選択されていない
                     //②それらのオブジェクトが隣接している
@@ -58,17 +68,25 @@ public class Chaincontroller : MonoBehaviour
                         hitObj.name == listLastObj.name)//③
                     {
                         m_ballList.Add(m_hit.collider.gameObject);
+
+                        //チェインの線を描く
+                        m_linePosList.Add(m_hit.collider.gameObject.transform.position);
+                        m_lineRenderer.positionCount++;
+                        m_lineRenderer.SetPositions(m_linePosList.ToArray());
                     }
 
-                    if (m_ballList.Count > 1 && hitObj == m_ballList[m_ballList.Count - 2])
+                    if (m_ballList.Count >= 2)
                     {
-                        listLastObj.gameObject.GetComponent<BallController2d>().UnSelectBall();
-                        m_ballList.RemoveAt(m_ballList.Count - 1);
-                    }
+                        //チェーン最中に戻した時に選択を解除する
+                        if (hitObj == m_ballList[m_ballList.Count - 2])
+                        {
+                            listLastObj.gameObject.GetComponent<BallController2d>().UnSelectBall();
+                            m_ballList.RemoveAt(m_ballList.Count - 1);
 
-                    if (hitObj.GetComponent<BallController2d>().m_isSelect)
-                    {
-
+                            //チェインの線を消す
+                            m_linePosList.RemoveAt(m_lineRenderer.positionCount - 1);
+                            m_lineRenderer.positionCount--;
+                        }
                     }
                 }
 
@@ -98,17 +116,10 @@ public class Chaincontroller : MonoBehaviour
             }
 
             m_ballList.Clear();
-        }
-    }
 
-    /// <summary>
-    /// 既に選択されているボールに戻ったときに、最後に選択したボールをListから消す
-    /// </summary>
-    void SelectBuck(int count, GameObject hitObj, List<GameObject> list)
-    {
-        for (int i = 0; i < count; i++)
-        {
-
+            //チェインの線を消す
+            m_linePosList.Clear();
+            m_lineRenderer.positionCount = 0;
         }
     }
 }
