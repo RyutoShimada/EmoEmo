2ae6e76ea9d6e09d6bcedfa5fa097af707828b86
yosho_cho <choyosho165152987@gmail.com>
2021-04-18 15:20:56 +0900

    メッセージBOXがチェイン数を受け取る関数を追加

---
+++ Assets/Scripts/Yosho/MessageText.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MessageText : MonoBehaviour
+{
+    Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+       GameObject gm = transform.Find("Text").gameObject;
+        text = gm.GetComponent<Text>();
+    }
+
+   
+    void Update()
+    {
+        
+    }
+
+    public void ChainText(int ChainCount)
+    {
+        text.text = ChainCount.ToString();
+    }
+    
+
+}
--- Assets/Scripts/Yosho/messagecontroller.cs
+++ Assets/Scripts/Yosho/messagecontroller.cs
@@ -6,7 +6,21 @@ public class messagecontroller : MonoBehaviour
 {
     List<GameObject> tagObjects;
 
-    [SerializeField] GameObject message;
+    [SerializeField] GameObject niyari;
+
+    [SerializeField] GameObject Buruburu;
+
+    [SerializeField] GameObject kyun;
+
+    [SerializeField] GameObject stamp;
+
+    [SerializeField] GameObject oko;
+
+    //[SerializeField] GameObject message;
+
+    Chaincontroller m_chain;
+
+    
 
     [SerializeField] Transform generatePosition;
 
@@ -17,19 +31,56 @@ public class messagecontroller : MonoBehaviour
         tagObjects = new List<GameObject>();
     }
 
-
-
-    public void Generate()
+    public void Generate(GameObject message)
     {
-        Debug.Log(tagObjects);
+        //GameObject messages = Instantiate(message, generatePosition, generatePosition);
         tagObjects.Add(message);
+
+        if (niyari.gameObject.layer == message.gameObject.layer) 
+        {
+            Debug.Log("niyari");
+            Instantiate(oko, generatePosition, generatePosition);
+        }
+        else if (Buruburu.gameObject.layer == message.gameObject.layer)
+        {
+            Debug.Log("Buruburu");
+            Instantiate(Buruburu, generatePosition, generatePosition);
+        }
+        else if (kyun.gameObject.layer == message.gameObject.layer)
+        {
+            Debug.Log("kyun");
+            Instantiate(kyun, generatePosition, generatePosition);
+        }
+        else if (stamp.gameObject.layer == message.gameObject.layer)
+        {
+            Debug.Log("stamp");
+            Instantiate(stamp, generatePosition, generatePosition);
+        }
+        else if (oko.gameObject.layer == message.gameObject.layer)
+        {
+            Debug.Log("oko");
+            Instantiate(oko, generatePosition, generatePosition);
+        }
+
         currentCount++;
-        Instantiate(tagObjects[currentCount - 1], generatePosition, generatePosition);
         if (currentCount > 4)
         {
-            tagObjects.RemoveAt(tagObjects.Count - 1);
             currentCount--;
-            //Destroy(tagObjects[tagObjects.Count - 1]);
+            Destroy(tagObjects[0]);
+            tagObjects.RemoveAt(0);
+        }
+    }
+
+    public void Stamp()
+    {
+        GameObject gm = Instantiate(stamp, generatePosition, generatePosition);
+        tagObjects.Add(gm);
+        currentCount++;
+        if (currentCount > 4)
+        {
+            currentCount--;
+            Destroy(tagObjects[0]);
+            tagObjects.RemoveAt(0);
         }
     }
 }
