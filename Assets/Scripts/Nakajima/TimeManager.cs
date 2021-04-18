21eaad2ce5265cb7633d57df456078c6eeaba5da
ShinNakajima128 <ukauka224@i.softbank.jp>
2021-04-18 17:52:38 +0900

    最終調整をした。

--- Assets/Scripts/Nakajima/TimeManager.cs
+++ Assets/Scripts/Nakajima/TimeManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class TimeManager : MonoBehaviour
 {
@@ -45,15 +46,15 @@ public class TimeManager : MonoBehaviour
             {
                 isPlayed = false;
                 m_timeupUI.SetActive(true);
-                StartCoroutine(DisplayButton());
+                StartCoroutine(LoadResult("Result"));
             }
         }
     }
 
-    IEnumerator DisplayButton()
+    IEnumerator LoadResult(string name)
     {
-        yield return new WaitForSeconds(2.0f);
+        yield return new WaitForSeconds(3.0f);
 
-        m_resultButton.SetActive(true);
+        SceneManager.LoadScene(name);
     }
 }
